0588d5639e023db0edb047a993ba983eef7add96
motoki9eslate-cell <motoki9eslate@gmail.com>
2026-07-05 09:37:04 +0900

    新規登録機能のバリデーションと遷移を実装

--- src/main/java/com/spring/springbootapplication/controller/TopController.java
+++ src/main/java/com/spring/springbootapplication/controller/TopController.java
@@ -2,6 +2,7 @@ package com.spring.springbootapplication.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class TopController {
@@ -14,5 +15,10 @@ public class TopController {
     @GetMapping("/register")
     public String register() {
         return "register";
+    }
+
+    @PostMapping("/register")
+    public String registerPost() {
+    return "redirect:/";
     }
 }
